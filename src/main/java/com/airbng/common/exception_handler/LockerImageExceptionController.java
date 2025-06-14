2250b0fbb3e8d46bfd71fa8031369cd1ac01b6b1
Mouon <ahemsapsldk@gmail.com>
2025-06-15 00:55:09 +0900

    feat : 예외 클래스들 작성
    예외 클래스 작성

---
+++ src/main/java/com/airbng/common/exception_handler/LockerImageExceptionController.java
@@ -0,0 +1,18 @@
+package com.airbng.common.exception_handler;
+
+import com.airbng.common.exception.LockerImageException;
+import com.airbng.common.response.BaseErrorResponse;
+import com.airbng.common.response.status.BaseExceptionResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class LockerImageExceptionController {
+    @ExceptionHandler(LockerImageException.class)
+    public ResponseEntity<BaseErrorResponse> handle(LockerImageException ex) {
+        return ResponseEntity
+                .status(BaseExceptionResponseStatus.FAILURE.getStatus())
+                .body(new BaseErrorResponse(BaseExceptionResponseStatus.FAILURE, ex.getMessage()));
+    }
+}
