fddea81915961b1192a13ea77eda0e213da8d4fc
yhee <wnn2011@naver.com>
2025-06-16 10:48:48 +0900

    feat : 검색 api 관련 파일 생성

---
+++ src/main/java/com/airbng/domain/LockerSearchResultDTO.java
@@ -0,0 +1,8 @@
+package com.airbng.domain;
+
+public class LockerSearchResultDTO {
+    private String lockerId;
+    private String locationId;
+    private String lockerName;
+    private String address;
+}
