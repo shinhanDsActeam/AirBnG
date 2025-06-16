f16111b61b068d6e16432151cba2dea20c8a7097
wocjs <friendly1217@naver.com>
2025-06-16 11:12:42 +0900

    feat: locker 등록 파일 작성
    service & controller 파일 작성

--- src/main/java/com/airbng/service/LockerService.java
+++ src/main/java/com/airbng/service/LockerService.java
@@ -1,7 +1,35 @@
 package com.airbng.service;
 
+import com.airbng.domain.Locker;
+import com.airbng.domain.Location;
+import com.airbng.domain.Member;
+import com.airbng.domain.base.Available;
+import com.airbng.dto.LockerDTO;
+import com.airbng.repository.LockerImageRepository;
+import com.airbng.repository.LockerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class LockerService {
 
+    private final LockerRepository lockerRepository;
+    private final LockerImageRepository lockerImageRepository;
 
+    @Transactional
+    public void registerLocker(LockerDTO dto) {
+        Locker locker = Locker.builder()
+                .isAvailable(Available.valueOf(dto.getIsAvailable()))
+                .location(Location.builder().locationId(dto.getLocationId()).build())
+                .owner(Member.builder().memberId(dto.getOwnerId()).build())
+                .build();
 
+        lockerRepository.insertLocker(locker); // lockerId가 채워짐
+
+        if (dto.getImageIds() != null && !dto.getImageIds().isEmpty()) {
+            lockerImageRepository.insertLockerImages(locker.getLockerId(), dto.getImageIds());
+        }
+    }
 }
