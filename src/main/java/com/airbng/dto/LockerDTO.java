2887bd8980d1f7a6e839ad6fa1a7567ad47d83e6
wocjs <friendly1217@naver.com>
2025-06-16 11:08:36 +0900

    feat: locker 등록 파일 작성
    lombok 어노테이션 추가

--- src/main/java/com/airbng/dto/LockerDTO.java
+++ src/main/java/com/airbng/dto/LockerDTO.java
@@ -2,9 +2,11 @@ package com.airbng.dto;
 
 import com.airbng.domain.LockerImage;
 import com.airbng.domain.base.Available;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class LockerDTO {
     private String isAvailable;       // enum Available -> String으로 받기 (예: "YES", "NO")
     private Long locationId;          // Location 객체 대신 ID
