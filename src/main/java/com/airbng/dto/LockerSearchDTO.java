2d8bd4408597ea0a83f795f95d8b611dbc2c880c
yhee <wnn2011@naver.com>
2025-06-17 09:35:56 +0900

    feat : locker 검색 dto 수정
    어노테이션 추가

--- src/main/java/com/airbng/dto/LockerSearchDTO.java
+++ src/main/java/com/airbng/dto/LockerSearchDTO.java
@@ -1,17 +1,22 @@
 package com.airbng.dto;
 
-import lombok.Data;
-
+import lombok.*;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LockerSearchDTO {
+    private int count;
     private List<LockerSearchDTO.Result> lockerList;
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Result {
-        private String locationId;
         private String address;
-        private String lockerId;
         private String lockerName;
         private String isAvailable;
         private String url;
