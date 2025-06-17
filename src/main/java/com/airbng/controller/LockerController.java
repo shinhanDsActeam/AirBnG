845192f3310035f3341652e0718ded31cba184e5
Mouon <ahemsapsldk@gmail.com>
2025-06-17 13:29:30 +0900

    feat : @RequestMapping("/locker") 추가

--- src/main/java/com/airbng/controller/LockerController.java
+++ src/main/java/com/airbng/controller/LockerController.java
@@ -6,11 +6,13 @@ import com.airbng.service.LockerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestController("/locker")
+@RestController
+@RequestMapping("/locker")
 public class LockerController {
 
     private final LockerService lockerService;
