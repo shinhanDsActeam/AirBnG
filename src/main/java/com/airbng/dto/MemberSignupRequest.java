db26cf2a4719ecb46a772ec2e1a4474f0d779f5c
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-06-17 14:59:47 +0900

    rename : DTO명 수정
    MemberSignupRequestDTO to MemberSignupRequest

--- src/main/java/com/airbng/dto/MemberSignupRequest.java
+++ src/main/java/com/airbng/dto/MemberSignupRequest.java
@@ -2,15 +2,13 @@ package com.airbng.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-public class MemberSignupRequestDTO {
+@Setter
+public class MemberSignupRequest {
     private String email;
     private String name;
     private String phone;
     private String nickname;
     private String password;
-
-    private MultipartFile profileImage;
 }
