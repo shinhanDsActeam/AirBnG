332515b0dc6d7a97f73d4804b43d6e53b3d5a055
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-17 11:45:01 +0900

    feat : 보관소 검색 쿼리문 수정
    @Mapper 추가, 총 건수 쿼리문 수정

--- src/main/java/com/airbng/mappers/LockerMapper.java
+++ src/main/java/com/airbng/mappers/LockerMapper.java
@@ -1,11 +1,12 @@
 package com.airbng.mappers;
 
 import com.airbng.dto.LockerSearchDTO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface LockerMapper {
-
     List<LockerSearchDTO.Result> lockerSearch(LockerSearchDTO.Result ls);
     int lockerSearchCount(LockerSearchDTO.Result ls);
 }
