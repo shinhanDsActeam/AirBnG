f03d161725bd45096649f84cb701b5ab695afea7
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-06-17 14:56:34 +0900

    feat : Member 생성 쿼리 작성

--- src/main/java/com/airbng/mappers/MemberMapper.java
+++ src/main/java/com/airbng/mappers/MemberMapper.java
@@ -1,8 +1,9 @@
 package com.airbng.mappers;
 
+import com.airbng.domain.Member;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-
+    void insertMember(Member member);
 }
