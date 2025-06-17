6c5b1d321d7d7c85eedab475b8f5574e4d33f630
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-06-17 14:57:42 +0900

    refactor : service interface 분리

--- src/main/java/com/airbng/service/MemberService.java
+++ src/main/java/com/airbng/service/MemberService.java
@@ -1,34 +1,8 @@
 package com.airbng.service;
 
-import com.airbng.common.exception.MemberException;
-import com.airbng.domain.Member;
-import com.airbng.domain.base.BaseStatus;
-import com.airbng.dto.MemberSignupRequestDTO;
-import com.airbng.mappers.MemberMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.airbng.dto.MemberSignupRequest;
+import org.springframework.web.multipart.MultipartFile;
 
-@Service
-@RequiredArgsConstructor
-public class MemberService {
-
-    private final MemberMapper memberMapper;
-
-    public void signup(MemberSignupRequestDTO dto) {
-        Long imageId;
-
-        //프로필 이미지 처리해야함
-        if(dto.getProfileImage() !=null && !dto.getProfileImage().isEmpty()) {
-        }
-
-        Member member = Member.builder()
-                .email(dto.getEmail())
-                .name(dto.getName())
-                .phone(dto.getPhone())
-                .nickname(dto.getNickname())
-                .password(dto.getPassword())
-                .status(BaseStatus.ACTIVE)
-                .profileImage()
-                .profileImageId
-    }
+public interface MemberService {
+    void signup(MemberSignupRequest dto, MultipartFile profileImage);
 }
---
+++ src/main/java/com/airbng/service/MemberServiceImpl.java
@@ -0,0 +1,6 @@
+package com.airbng.service;
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceImpl implements MemberService {
+}
