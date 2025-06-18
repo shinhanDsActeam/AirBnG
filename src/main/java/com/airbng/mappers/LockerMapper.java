9fe74c6e0eaa1e2af9bb54d7ebcb24a4d4b540c5
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-18 16:04:19 +0900

    feat : 보관소 검색 쿼리문 수정
    짐 종류별 검색 쿼리문 추가, 검색 총 건수 쿼리문 수정

--- src/main/java/com/airbng/mappers/LockerMapper.java
+++ src/main/java/com/airbng/mappers/LockerMapper.java
@@ -1,12 +1,21 @@
 package com.airbng.mappers;
 
-import com.airbng.dto.LockerSearchDTO;
+import com.airbng.dto.LockerPreviewResult;
+import com.airbng.dto.LockerSearchRequest;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface LockerMapper {
-    List<LockerSearchDTO.Result> lockerSearch(LockerSearchDTO.Result ls);
-    int lockerSearchCount(LockerSearchDTO.Result ls);
+    List<LockerPreviewResult> findAllLockerBySearch(LockerPreviewResult lpr);
+    String findOneImageById(Long lockerId);
+    Long findLockerCount(LockerPreviewResult lsr);
+
+    // 1. Locker 검색
+    List<LockerPreviewResult> findAllLockerBySearch(LockerSearchRequest condition);
+
+    // 2. Count 조회
+    Long findLockerCount(LockerSearchRequest condition);
 }
