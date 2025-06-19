e123f4cff008ba733d6d38f39777783e18e2ae05
wocjs <friendly1217@naver.com>
2025-06-19 17:41:35 +0900

    feat : 로그인 기능 구현

--- src/main/java/com/airbng/mappers/MemberMapper.java
+++ src/main/java/com/airbng/mappers/MemberMapper.java
@@ -2,6 +2,7 @@ package com.airbng.mappers;
 
 import com.airbng.domain.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MemberMapper {
@@ -9,4 +10,5 @@ public interface MemberMapper {
     boolean findByEmail(String email);
     boolean findByNickname(String nickname);
     boolean findByPhone(String phone);
+    Member findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 }
