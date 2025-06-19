c1a98ef525fc36ae5338bbd395af225891ec3a4a
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-06-19 14:43:12 +0900

    test : 테스트 코드 작성

--- src/main/java/com/airbng/dto/MemberSignupRequest.java
+++ src/main/java/com/airbng/dto/MemberSignupRequest.java
@@ -1,10 +1,12 @@
 package com.airbng.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MemberSignupRequest {
     private String email;
     private String name;
