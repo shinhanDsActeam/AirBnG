a92aa20420659b993fd5ecf4bc0986a236efad92
Your Name <you@example.com>
2025-06-19 14:31:36 +0900

    rename : 보관소 상세 조회 테스트 이름 변경

--- src/test/java/com/airbng/service/LockerServiceImplTest.java
+++ src/test/java/com/airbng/service/LockerServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LockerServiceImplTest {
+class LockerServiceTest {
 
     @InjectMocks
     private LockerServiceImpl lockerService;
