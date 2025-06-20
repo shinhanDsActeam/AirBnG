1b2b7d3e6d6028062ad93f44ba415d6245c80753
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-06-20 16:13:46 +0900

    style : BaseResponseStatus 코드 순서 정리

--- src/main/java/com/airbng/common/response/status/BaseResponseStatus.java
+++ src/main/java/com/airbng/common/response/status/BaseResponseStatus.java
@@ -18,7 +18,7 @@ public enum BaseResponseStatus implements ResponseStatus{
     /**
      * 2000 맴버 관련 코드
      */
-    NOT_FOUND_MEMBER(2001, HttpStatus.NOT_FOUND.value(), "멤버를 찾을 수 없습니다."),
+    NOT_FOUND_MEMBER(2001, HttpStatus.NOT_FOUND.value(), "존재하지 않는 멤버입니다."),
     DUPLICATE_EMAIL(2002,HttpStatus.BAD_REQUEST.value(), "중복이메일 사용"),
     DUPLICATE_NICKNAME(2003,HttpStatus.BAD_REQUEST.value(),"중복닉네임 사용"),
     INVALID_PASSWORD(2004,HttpStatus.BAD_REQUEST.value(),"비밀번호 형식 오류"),
@@ -32,7 +32,6 @@ public enum BaseResponseStatus implements ResponseStatus{
     NOT_FOUND_LOCKER(3000,HttpStatus.BAD_REQUEST.value(), "락커를 찾을 수 없습니다."),
     NOT_FOUND_LOCKERDETAILS(3001,HttpStatus.BAD_REQUEST.value(), "락커를 찾을 수 없습니다."),
     MEMBER_ALREADY_HAS_LOCKER(3002, HttpStatus.BAD_REQUEST.value(), "한 멤버당 하나의 보관소만 등록할 수 있습니다."),
-    DUPLICATE_JIMTYPE(3003, HttpStatus.BAD_REQUEST.value(), "중복된 짐 타입이 존재합니다."),
     LOCKER_KEEPER_MISMATCH(3004, HttpStatus.BAD_REQUEST.value(), "선택한 보관소의 보관자 정보가 일치하지 않습니다."),
     /**
      * 4000 예약 관련 코드
@@ -48,10 +47,9 @@ public enum BaseResponseStatus implements ResponseStatus{
      * 5000 짐 타입 관련 코드
      */
     INVALID_JIMTYPE(5000, HttpStatus.UNPROCESSABLE_ENTITY.value(), "존재하지 않는 짐 타입이 포함되어 있습니다."),
-    NOT_FOUND_JIMTYPE(5001, HttpStatus.NOT_FOUND.value(), "짐 타입을 찾을 수 없습니다."),
-    INVALID_JIMTYPE_COUNT(5002, HttpStatus.BAD_REQUEST.value(), "요청한 짐 타입 개수와 실제 등록된 개수가 일치하지 않습니다."),
-    LOCKER_DOES_NOT_SUPPORT_JIMTYPE(5003, HttpStatus.BAD_REQUEST.value(), "해당 보관소가 관리하지 않는 짐 타입입니다."),
-
+    INVALID_JIMTYPE_COUNT(5001, HttpStatus.BAD_REQUEST.value(), "요청한 짐 타입 개수와 실제 등록된 개수가 일치하지 않습니다."),
+    LOCKER_DOES_NOT_SUPPORT_JIMTYPE(5002, HttpStatus.BAD_REQUEST.value(), "해당 보관소가 관리하지 않는 짐 타입입니다."),
+    DUPLICATE_JIMTYPE(5003, HttpStatus.BAD_REQUEST.value(), "중복된 짐 타입이 존재합니다."),
     /**
      * 6000: image
      */
