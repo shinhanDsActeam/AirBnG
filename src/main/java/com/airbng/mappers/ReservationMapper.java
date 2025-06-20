296a779bf81fe00803b2567c3e7213f4ef137240
Mouon <ahemsapsldk@gmail.com>
2025-06-20 15:04:18 +0900

    feat : ReservationMapper 클래스 추가

---
+++ src/main/java/com/airbng/mappers/ReservationMapper.java
@@ -0,0 +1,10 @@
+package com.airbng.mappers;
+
+import com.airbng.domain.Reservation;
+import com.airbng.domain.base.ReservationState;
+import org.apache.ibatis.annotations.Param;
+
+public interface ReservationMapper {
+    void updateReservationState(@Param("reservationId")Long reservationId, @Param("state")ReservationState state);
+    Reservation findReservationById(Long reservationId);
+}
