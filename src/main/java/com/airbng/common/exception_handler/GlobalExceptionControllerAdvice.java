5788f4071c8e3207a1b1a4be3390c37536ac2713
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-06-23 03:13:26 +0900

    refactor : DateTimeParseException 예외 처리 개선
    - LocalDateTimeUtils.parse() 메서드에서 DateTimeParseException을 그대로 던지도록 변경
    - DateTimeParseException을 핸들링 시 응답 포맷 통일
      - BaseResponseStatus로 관리

--- src/main/java/com/airbng/common/exception_handler/GlobalExceptionControllerAdvice.java
+++ src/main/java/com/airbng/common/exception_handler/GlobalExceptionControllerAdvice.java
@@ -1,29 +1,36 @@
 package com.airbng.common.exception_handler;
 
 import com.airbng.common.exception.DomainException;
-import com.airbng.common.exception.ImageException;
 import com.airbng.common.response.BaseErrorResponse;
-import com.airbng.common.response.status.BaseResponseStatus;
-import org.springframework.http.HttpStatus;
+import com.airbng.common.response.FieldValidationError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.format.DateTimeParseException;
+
+import static com.airbng.common.response.status.BaseResponseStatus.INVALID_DATETIME_FORMAT;
 
 @RestControllerAdvice
 public class GlobalExceptionControllerAdvice {
 
     @ExceptionHandler(DomainException.class)
-    public ResponseEntity<BaseErrorResponse> handle(DomainException ex) {
+    public ResponseEntity<BaseErrorResponse> handleDomainException(DomainException ex) {
         return ResponseEntity
                 .status(ex.getBaseResponseStatus().getHttpStatus())
                 .body(new BaseErrorResponse(ex.getBaseResponseStatus(), ex.getMessage()));
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<BaseErrorResponse> handle(IllegalArgumentException ex) {
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<BaseErrorResponse> handleDateTimeParseException(DateTimeParseException ex) {
+        FieldValidationError error = FieldValidationError.builder()
+                .fieldName("dateTime")
+                .rejectValue(ex.getParsedString())
+                .message(ex.getMessage())
+                .build();
+
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new BaseErrorResponse(BaseResponseStatus.FAILURE, ex.getMessage()));
+                .status(INVALID_DATETIME_FORMAT.getHttpStatus())
+                .body(new BaseErrorResponse(INVALID_DATETIME_FORMAT, error));
     }
 }
