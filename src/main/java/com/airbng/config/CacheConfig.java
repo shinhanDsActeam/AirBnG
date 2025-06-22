1f1713804e46cf6b80443ed2cde262f33ed98d9f
Mouon <ahemsapsldk@gmail.com>
2025-06-22 18:38:12 +0900

    feat : Caffeine Cache 에 scheduler 명시
    기본적으로 event-driven eviction 떄문에 스케줄러를 명시해야 -> 백그라운드에서 주기적 evict 발생

--- src/main/java/com/airbng/config/CacheConfig.java
+++ src/main/java/com/airbng/config/CacheConfig.java
@@ -2,6 +2,7 @@ package com.airbng.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Scheduler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +11,15 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @Configuration
 public class CacheConfig {
-
+    /**
+     * 기본적으로 event-driven eviction 떄문에
+     * 스케줄러를 명시해야 -> 백그라운드에서 주기적 evict 발생
+     * */
     @Bean
     public Cache<Long, ReentrantLock> reservationLocks() {
         return Caffeine.newBuilder()
                 .expireAfterAccess(10, TimeUnit.MINUTES)
+                .scheduler(Scheduler.systemScheduler())
                 .build();
     }
 }
