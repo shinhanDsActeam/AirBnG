3a572f6b912f5dc3e490b3426933ff0be5c0481b
wocjs <friendly1217@naver.com>
2025-06-23 12:29:34 +0900

    refactor : email 형식 검사 강화

--- src/main/java/com/airbng/service/MemberServiceImpl.java
+++ src/main/java/com/airbng/service/MemberServiceImpl.java
@@ -70,7 +70,8 @@ public class MemberServiceImpl implements MemberService {
     //이메일 형식 체크
     private boolean isValidEmail(String email) {
         //@앞의 문자 1개이상, @ 뒤에 문자+ . + 2~6자(com, net, co,kr , email)
-        return email != null && email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$");
+        // 점(.)이 연속되거나, @ 앞/뒤 형식이 잘못된 경우 모두 차단
+        return email != null && email.matches("^[A-Za-z0-9]+([._%+-]?[A-Za-z0-9]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.[A-Za-z]{2,6}$");
     }
 
     @Override
@@ -86,7 +87,7 @@ public class MemberServiceImpl implements MemberService {
 
             return MemberLoginResponse.from(member);
         } catch (NullPointerException e) {
-            throw new MemberException(BaseResponseStatus.INVALID_MEMBER);
+            throw new MemberException(INVALID_MEMBER);
         }
     }
 
