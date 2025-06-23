743dd1b8c1a0029dc5eda9fac10c8f8db680f4ff
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-23 11:52:49 +0900

    feat : 회원 정보 조회 퀴리문 작성

--- src/main/java/com/airbng/mappers/MemberMapper.java
+++ src/main/java/com/airbng/mappers/MemberMapper.java
@@ -1,7 +1,11 @@
 package com.airbng.mappers;
 
 import com.airbng.domain.Member;
+import com.airbng.dto.MemberMyPageResult;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MemberMapper {
@@ -9,4 +13,5 @@ public interface MemberMapper {
     boolean findByEmail(String email);
     boolean findByNickname(String nickname);
     boolean findByPhone(String phone);
+    MemberMyPageResult findUserById(@Param("memberId") Long memberId);
 }
