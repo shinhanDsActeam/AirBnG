64cd284948a344aeffb5fecb47c213905d108473
SeoHa <90242821+standha@users.noreply.github.com>
2025-06-23 14:56:52 +0900

    fix : JimTypeResult에 Builder 추가

--- src/main/java/com/airbng/dto/jimType/JimTypeResult.java
+++ src/main/java/com/airbng/dto/jimType/JimTypeResult.java
@@ -1,12 +1,10 @@
 package com.airbng.dto.jimType;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class JimTypeResult {
