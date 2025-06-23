44bd803194ac8e2f81539e0fc44af3ae4a29f9b7
SeoHa <90242821+standha@users.noreply.github.com>
2025-06-23 14:55:14 +0900

    Merge devleop
    This reverts commit 5d7faa43567489daa2b130080a79f865e2bdc567, reversing
    changes made to 49cdce91dc0564d9fbe4a4fe69cb3bb32ccf02fb.
    
    # Conflicts:
    #	pom.xml
    
    # Conflicts:
    #	src/test/java/com/airbng/service/LockerServiceTest.java

--- src/main/java/com/airbng/dto/jimType/JimTypeResult.java
+++ src/main/java/com/airbng/dto/jimType/JimTypeResult.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Builder;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class JimTypeResult {
