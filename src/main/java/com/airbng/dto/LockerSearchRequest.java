610ac694e118271654d9d3031b7d65ada8b7825c
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-06-24 15:35:43 +0900

    fix : 충돌해결

---
+++ src/main/java/com/airbng/dto/LockerSearchRequest.java
@@ -0,0 +1,16 @@
+package com.airbng.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LockerSearchRequest {
+    private String address;
+    private String lockerName;
+    private List<Long> jimTypeId;  // 서버에서 조회 후 채움
+}
