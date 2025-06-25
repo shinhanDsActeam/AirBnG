512b36ef4f98723238f3a1618efd63755743ece4
Mouon <ahemsapsldk@gmail.com>
2025-06-25 16:15:07 +0900

    feat : 유효성 추가

--- src/main/java/com/airbng/controller/ReservationController.java
+++ src/main/java/com/airbng/controller/ReservationController.java
@@ -5,13 +5,17 @@ import com.airbng.dto.reservation.ReservationDetailResponse;
 import com.airbng.dto.reservation.ReservationInsertRequest;
 import com.airbng.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
+@Validated
 public class ReservationController {
     private final ReservationService reservationService;
     // 예약 등록
@@ -21,7 +25,9 @@ public class ReservationController {
     }
 
     @GetMapping("{reservation-id}/members/{member-id}/detail")
-    public BaseResponse<ReservationDetailResponse> getReservationDetail(@PathVariable("reservation-id") Long reservationId, @PathVariable("member-id") Long memberId){
+    public BaseResponse<ReservationDetailResponse> getReservationDetail(
+            @PathVariable("reservation-id")  @NotNull @Min(1) Long reservationId,
+            @PathVariable("member-id") @NotNull @Min(1) Long memberId){
         return new BaseResponse<>(reservationService.findReservationDetail(reservationId, memberId));
     }
 
