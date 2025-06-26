d42aa2a630679ecceedd2706ecfbec9035fceb25
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-26 11:52:21 +0900

    feat : 회원정보 수정 쿼리문 수정 중
    회원정보 수정 가능 (이미지 url만 빼고)

--- src/main/java/com/airbng/mappers/MemberMapper.java
+++ src/main/java/com/airbng/mappers/MemberMapper.java
@@ -3,8 +3,10 @@ package com.airbng.mappers;
 import com.airbng.domain.Member;
 import com.airbng.dto.MemberMyPageRequest;
 import com.airbng.dto.MemberMyPageResponse;
+import com.airbng.dto.MemberUpdateRequest;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,9 +20,12 @@ public interface MemberMapper {
 
     boolean findByPhone(String phone);
 
-    MemberMyPageResponse findUserById(@Param("memberId") Long memberId);
-
     Member findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
     boolean isExistMember(Long memberId);
+
+    MemberMyPageResponse findUserById(@Param("memberId") Long memberId);
+
+    //MemberMyPageResponse updateUserById(MemberUpdateRequest memberUpdateRequest);
+    int updateUserById(MemberUpdateRequest memberUpdateRequest);
 }
