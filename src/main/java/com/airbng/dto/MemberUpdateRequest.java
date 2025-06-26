fcc91003f1319588514308fe3607a5c7c66c6d87
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-06-26 11:52:55 +0900

    feat : 회원정보 수정 dto

---
+++ src/main/java/com/airbng/dto/MemberUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.airbng.dto;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberUpdateRequest {
+    private Long memberId;
+    private String email;
+    private String name;
+    private String phone;
+    private String nickname;
+    private String profileImageUrl;
+}
