28b509114eea2d319c11dc2e26a38cb8ebc56f73
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-06-27 15:57:18 +0900

    refactor : 검증 메소드 분리

--- src/main/java/com/airbng/service/ReservationServiceImpl.java
+++ src/main/java/com/airbng/service/ReservationServiceImpl.java
@@ -5,9 +5,9 @@ import com.airbng.common.exception.LockerException;
 import com.airbng.common.exception.MemberException;
 import com.airbng.common.exception.ReservationException;
 import com.airbng.common.response.status.BaseResponseStatus;
-import com.airbng.domain.base.ReservationState;
 import com.airbng.domain.Reservation;
 import com.airbng.domain.base.ChargeType;
+import com.airbng.domain.base.ReservationState;
 import com.airbng.dto.jimType.JimTypeCountResult;
 import com.airbng.dto.reservation.*;
 import com.airbng.mappers.JimTypeMapper;
@@ -31,13 +31,13 @@ import static com.airbng.common.response.status.BaseResponseStatus.*;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ReservationServiceImpl implements ReservationService{
-  
+public class ReservationServiceImpl implements ReservationService {
+
     private final ReservationMapper reservationMapper;
     private final JimTypeMapper jimTypeMapper;
     private final MemberMapper memberMapper;
     private final LockerMapper lockerMapper;
-    private static final Long LIMIT= 10L; // 페이지당 최대 예약 개수
+    private static final Long LIMIT = 10L; // 페이지당 최대 예약 개수
 
     //예약 조회 + 페이징 처리
 
@@ -47,7 +47,7 @@ public class ReservationServiceImpl implements ReservationService{
                 memberId, role, state, nextCursorId, LIMIT, period);
 
         // 초기 커서 ID 설정
-        if(nextCursorId == null) {
+        if (nextCursorId == null) {
             nextCursorId = -1L;
         }
 
@@ -105,14 +105,15 @@ public class ReservationServiceImpl implements ReservationService{
             lock.lock();
 
             /** 맴버 존재 유무 파악 */
-            if(!memberMapper.findById(memberId)) throw new MemberException(NOT_FOUND_MEMBER);
+            if (!memberMapper.findById(memberId)) throw new MemberException(NOT_FOUND_MEMBER);
 
             /** 요청 예약건의 존재여부 파악 */
             Reservation reservation = reservationMapper.findReservationWithDropperById(reservationId);
             if (reservation == null) throw new ReservationException(NOT_FOUND_RESERVATION);
 
             /** 예약건의 주인이 맞는지 파악 */
-            if(!reservation.getDropper().getMemberId().equals(memberId)) throw new ReservationException(NOT_DROPPER_OF_RESERVATION);
+            if (!reservation.getDropper().getMemberId().equals(memberId))
+                throw new ReservationException(NOT_DROPPER_OF_RESERVATION);
 
             ChargeType chargeType = ChargeType.from(reservation.getStartTime());
             ReservationState state = reservation.getState();
@@ -121,13 +122,13 @@ public class ReservationServiceImpl implements ReservationService{
             reservationMapper.updateReservationState(reservationId,
                     ReservationState.CANCELLED);
 
-             return ReservationCancelResponse.of(reservation,
-                     chargeType.discountAmount(),ReservationState.CANCELLED);
+            return ReservationCancelResponse.of(reservation,
+                    chargeType.discountAmount(), ReservationState.CANCELLED);
 
-            } finally{
-                /** 무조건 락 해제 */
-                lock.unlock();
-            }
+        } finally {
+            /** 무조건 락 해제 */
+            lock.unlock();
+        }
     }
 
     @Override
@@ -145,7 +146,8 @@ public class ReservationServiceImpl implements ReservationService{
             if (reservation == null) throw new ReservationException(NOT_FOUND_RESERVATION);
 
             //짐을 맡아주는 사람인지 확인
-            if (!reservation.getKeeper().getMemberId().equals(memberId)) throw new ReservationException(NOT_KEEPER_OF_RESERVATION);
+            if (!reservation.getKeeper().getMemberId().equals(memberId))
+                throw new ReservationException(NOT_KEEPER_OF_RESERVATION);
 
             //취소, 완료상태는 상태변경 불가
             reservation.getState().isAvailableUpdate(reservation.getState());
@@ -167,86 +169,74 @@ public class ReservationServiceImpl implements ReservationService{
         }
     }
 
+
+    @Override
+    public ReservationDetailResponse findReservationDetail(Long reservationId, Long memberId) {
+        Reservation reservation = reservationMapper.findReservationDetailById(reservationId);
+        if (reservation == null) throw new ReservationException(NOT_FOUND_RESERVATION); // 보관소 있나요
+        if (!reservation.getDropper().getMemberId().equals(memberId))
+            throw new ReservationException(NOT_DROPPER_OF_RESERVATION); // 있는 보관소가 내거 맞나요
+
+        return ReservationDetailResponse.from(reservation);
+    }
+
     // 예약 등록
     @Override
     @Transactional // 짐타입 등록 실패한 경우 예약 등록까지 롤백
-    public BaseResponseStatus insertReservation(ReservationInsertRequest request) {
+    public BaseResponseStatus insertReservation(final ReservationInsertRequest request) {
         log.info("insertReservation({})", request);
 
-        // startTime과 endTime이 유효한지 확인
-        String startTime = request.getStartTime();
-        String endTime = request.getEndTime();
-        if (request.getStartTime() == null || request.getEndTime() == null) {
-            throw new ReservationException(INVALID_RESERVATION_TIME);
-        }
+        validateStartTimeAndEndTime(request.getStartTime(), request.getEndTime());
+        validateMember(request.getDropperId(), request.getKeeperId());
+        validateLockerKeeper(request.getLockerId(), request.getKeeperId());
+        validateJimTypes(request.getLockerId(), request.getJimTypeCounts());
 
-        if (LocalDateTimeUtils.isStartTimeAfterEndTime(startTime, endTime)
-                || LocalDateTimeUtils.isStartTimeEqualEndTime(startTime, endTime)) {
-            throw new ReservationException(INVALID_RESERVATION_TIME_ORDER);
-        }
-
-        // dropper와 keeper 존재 여부 확인
-        boolean dropperFlag = memberMapper.isExistMember(request.getDropperId());
-        if (!dropperFlag) {
-            throw new MemberException(NOT_FOUND_MEMBER);
-        }
-
-        boolean keeperFlag = memberMapper.isExistMember(request.getKeeperId());
-        if (!keeperFlag) {
-            throw new MemberException(NOT_FOUND_MEMBER);
-        }
-
-        // dropper와 keeper가 동일한 경우 예외
-        if (request.getDropperId().equals(request.getKeeperId())) {
-            throw new ReservationException(INVALID_RESERVATION_PARTICIPANTS);
-        }
-
-        // 락커 존재 여부 확인
-        boolean lockerFlag = lockerMapper.isExistLocker(request.getLockerId());
-        if (!lockerFlag) {
-            throw new LockerException(NOT_FOUND_LOCKER);
-        }
-
-        // keeper가 락커의 소유자인지 확인
-        boolean isLockerKeeper = lockerMapper.isLockerKeeper(request.getLockerId(), request.getKeeperId());
-        if (!isLockerKeeper) {
-            throw new LockerException(LOCKER_KEEPER_MISMATCH);
-        }
-
-        // 예약 엔티티 추가
         reservationMapper.insertReservation(request);
-        Long reservationId = request.getId();
-        if (reservationId == null || reservationId < 1) {
-            throw new ReservationException(CANNOT_CREATE_RESERVATION);
-        }
+        int cnt = jimTypeMapper.insertReservationJimTypes(request.getId(), request.getJimTypeCounts());
 
-        // 해당 보관소가 관리하는 짐타입들인지 검사
-        List<Long> jimTypeIds = request.getJimTypeCounts().stream()
-                .map(JimTypeCountResult::getJimTypeId)
-                .collect(Collectors.toList());
-        boolean validateJimtype = jimTypeMapper.validateLockerJimTypes(request.getLockerId(), jimTypeIds, jimTypeIds.size());
-        if (!validateJimtype) {
-            throw new JimTypeException(LOCKER_DOES_NOT_SUPPORT_JIMTYPE);
-        }
-
-
-        // 위에서 insert한 예약 엔티티에 맞게 짐 타입 등록
-        int cnt = jimTypeMapper.insertReservationJimTypes(reservationId, request.getJimTypeCounts());
-        // 요청한 짐 타입 개수와 실제 등록된 개수가 일치하지 않는 경우 예외
-        // Mapper 에서 메서드가 실패하면 0을 반환
         if (cnt != request.getJimTypeCounts().size()) {
             throw new ReservationException(INVALID_JIMTYPE_COUNT);
         }
 
         return CREATED_RESERVATION;
     }
-    @Override
-    public ReservationDetailResponse findReservationDetail(Long reservationId, Long memberId) {
-        Reservation reservation = reservationMapper.findReservationDetailById(reservationId);
-        if(reservation==null) throw new ReservationException(NOT_FOUND_RESERVATION); // 보관소 있나요
-        if(!reservation.getDropper().getMemberId().equals(memberId)) throw new ReservationException(NOT_DROPPER_OF_RESERVATION); // 있는 보관소가 내거 맞나요
 
-        return ReservationDetailResponse.from(reservation);
+    private static void validateStartTimeAndEndTime(final String startTime, final String endTime) {
+        if (LocalDateTimeUtils.isStartTimeAfterEndTime(startTime, endTime)
+                || LocalDateTimeUtils.isStartTimeEqualEndTime(startTime, endTime)) {
+            throw new ReservationException(INVALID_RESERVATION_TIME_ORDER);
+        }
     }
 
+    private void validateJimTypes(final Long lockerId, final List<JimTypeCountResult> jimTypeCounts) {
+        List<Long> jimTypeIds = jimTypeCounts.stream()
+                .map(JimTypeCountResult::getJimTypeId)
+                .collect(Collectors.toList());
+        if (!jimTypeMapper.validateLockerJimTypes(lockerId, jimTypeIds, jimTypeIds.size())) {
+            throw new JimTypeException(LOCKER_DOES_NOT_SUPPORT_JIMTYPE);
+        }
+    }
+
+    private void validateLockerKeeper(final Long lockerId, final Long keeperId) {
+        if (!lockerMapper.isExistLocker(lockerId)) {
+            throw new LockerException(NOT_FOUND_LOCKER);
+        }
+        if (!lockerMapper.isLockerKeeper(lockerId, keeperId)) {
+            throw new LockerException(LOCKER_KEEPER_MISMATCH);
+        }
+    }
+
+    private void validateMember(final Long dropperId, final Long keeperId) {
+        // dropper와 keeper가 동일한 경우 예외
+        if (dropperId.equals(keeperId)) {
+            throw new ReservationException(INVALID_RESERVATION_PARTICIPANTS);
+        }
+        // dropper와 keeper 존재 여부 확인
+        if (!memberMapper.isExistMember(dropperId)) {
+            throw new MemberException(NOT_FOUND_MEMBER);
+        }
+        if (!memberMapper.isExistMember(keeperId)) {
+            throw new MemberException(NOT_FOUND_MEMBER);
+        }
+    }
 }
