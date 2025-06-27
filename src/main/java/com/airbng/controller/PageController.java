58f27e4254afb09659a3abed72fe79b428dedafb
standha <90242821+standha@users.noreply.github.com>
2025-06-27 17:46:23 +0900

    feat : 알림 발송 테스트 페이지 추가

---
+++ src/main/java/com/airbng/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.airbng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/alarms/sse-test")
+    public String redirectToStaticHtml() {
+        return "redirect:/alarm.html";
+    }
+}
