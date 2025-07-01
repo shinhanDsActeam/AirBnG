bffea402201fd3fb5519bf7b90750705d1fb856a
Mouon <ahemsapsldk@gmail.com>
2025-07-01 11:21:38 +0900

    feat : ReservationPageController 추가

---
+++ src/main/java/com/airbng/controller/ReservationPageController.java
@@ -0,0 +1,14 @@
+package com.airbng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page/reservation")
+public class ReservationPageController {
+    @GetMapping("/detail")
+    public String index() {
+        return "reservationDetail";
+    }
+}
