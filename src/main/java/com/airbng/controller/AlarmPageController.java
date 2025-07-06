2a61998123cdc5404cd36df1859cf7dabec64632
standha <myyun02@naver.com>
2025-07-06 21:52:03 +0900

    fix : 알림 읽음/안읽음 확인 위한 코드 구현

--- src/main/java/com/airbng/controller/AlarmPageController.java
+++ src/main/java/com/airbng/controller/AlarmPageController.java
@@ -1,5 +1,9 @@
 package com.airbng.controller;
 
+import com.airbng.service.MemberService;
+import com.airbng.service.ReservationAlarmSseService;
+import com.airbng.service.ReservationAlarmSseServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +12,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/page")
 public class AlarmPageController {
+
+    private final ReservationAlarmSseService reservationAlarmSseService;
+
     @GetMapping("/notification")
-    public String notificationPage() {
+    public String notificationPage(HttpSession session) {
+
+        Long memberId = (Long) session.getAttribute("memberId");
+
+        if (memberId != null) {
+            reservationAlarmSseService.markAllAsRead(memberId);   //알림 읽음 처리
+            session.setAttribute("hasUnreadAlarm", false);  // dot 제거
+        }
+
         return "notification";
     }
 
--- src/main/java/com/airbng/controller/HomePageController.java
+++ src/main/java/com/airbng/controller/HomePageController.java
@@ -3,12 +3,15 @@ package com.airbng.controller;
 import com.airbng.dto.locker.LockerPreviewResult;
 import com.airbng.domain.base.ReservationState;
 import com.airbng.mappers.LockerMapper;
+import com.airbng.service.ReservationAlarmSseService;
+import com.airbng.service.ReservationAlarmSseServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -16,10 +19,17 @@ import java.util.List;
 @RequestMapping("/page")
 public class HomePageController {
 
-    private final LockerMapper lockerMapper;
+    private final ReservationAlarmSseService reservationAlarmSseService;
 
     @GetMapping("/home")
-    public String home() {
+    public String home(HttpSession session) {
+        Long memberId = (Long) session.getAttribute("memberId");
+
+        if (memberId != null) {
+            boolean hasUnread = reservationAlarmSseService.hasUnreadAlarm(memberId);
+            session.setAttribute("hasUnreadAlarm", hasUnread);
+        }
+
         return "home";
     }
 
