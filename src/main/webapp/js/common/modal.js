09e207dd7d7f2950b050ec16f874ed6f23a014b8
wocjs <friendly1217@naver.com>
2025-07-08 15:02:58 +0900

    refactor : 모달 수정

--- src/main/webapp/js/common/modal.js
+++ src/main/webapp/js/common/modal.js
@@ -84,7 +84,6 @@ class ModalUtils {
         }
     }
 
-
     /**
      * 성공 모달 표시
      * @param {string} message - 성공 메시지
@@ -151,7 +150,7 @@ class ModalUtils {
      * @param {Function} onCancel - 취소 콜백
      * @param {string} title - 확인 모달 타이틀
      */
-    static showConfirm(message = '정말로 진행하시겠습니까?', title = '확인', onConfirm = null, onCancel = null) {
+    static showConfirm(title = '확인', message = '정말로 진행하시겠습니까?', onConfirm = null, onCancel = null) {
         this.showModal('confirm-modal', {
             title: title,
             message: message,
