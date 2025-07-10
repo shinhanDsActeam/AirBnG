cc79acaff9dce5504bbec8be353674ba0af4cb21
wocjs <friendly1217@naver.com>
2025-07-10 15:57:40 +0900

    feat : BE 보관소 수정 기능 개발

---
+++ src/main/java/com/airbng/dto/locker/LockerUpdateRequest.java
@@ -0,0 +1,48 @@
+package com.airbng.dto.locker;
+
+import com.airbng.domain.base.Available;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LockerUpdateRequest {
+    @NonNull
+    private Long lockerId;
+
+    @NonNull
+    private String lockerName;
+
+    @NonNull
+    private Available isAvailable; // 'YES' or 'NO'
+
+    @NonNull
+    private Long keeperId;
+
+    @NonNull
+    private String address;
+
+    private String addressEnglish;
+
+    private String addressDetail;
+
+    @NonNull
+    private Double latitude;
+
+    @NonNull
+    private Double longitude;
+
+    @JsonIgnore
+    private List<MultipartFile> images;
+
+    private List<Long> jimTypeIds;
+}
