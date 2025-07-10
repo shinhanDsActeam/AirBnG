645994f8d12a2c4b283a980538ad07b725055078
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-07-10 18:14:36 +0900

    HOTFIX : modal util 원상복구
    모달 동작에 영향을 주는 util 함수의 잘못된 변경 사항을
    기존 로직으로 되돌림

--- src/main/webapp/js/common/modal.js
+++ src/main/webapp/js/common/modal.js
@@ -86,18 +86,16 @@ class ModalUtils {
 
     /**
      * 성공 모달 표시 (Promise 기반)
-     * @param {string} message
-     * @param {string} title
-     * @returns {Promise<void>}
+     * @param {string} message - 성공 메시지
+     * @param {string} title - 성공 타이틀
+     * @param {Function} callback - 콜백 함수
      */
-    static showSuccess(message = '작업이 성공적으로 완료되었습니다.', title = '성공') {
-        return new Promise((resolve) => {
-            this.showModal('success-modal', {
-                title: title,
-                message: message,
-                showCancel: false,
-                onConfirm: resolve
-            });
+    static showSuccess(message = '작업이 성공적으로 완료되었습니다.', title = '성공', callback = null) {
+        this.showModal('success-modal', {
+            title: title,
+            message: message,
+            onConfirm: callback,
+            showCancel: false
         });
     }
 
