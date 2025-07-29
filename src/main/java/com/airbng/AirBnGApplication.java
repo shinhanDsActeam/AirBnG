4074c884c83764fdeb31ef9b4501fd4ac634b3f9
Mouon <ahemsapsldk@gmail.com>
2025-07-29 16:34:18 +0900

    feat : 트랜조정 및 @EnableJpaAuditing 추가

--- src/main/java/com/airbng/AirBnGApplication.java
+++ src/main/java/com/airbng/AirBnGApplication.java
@@ -2,8 +2,11 @@ package com.airbng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AirBnGApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirBnGApplication.class, args);
