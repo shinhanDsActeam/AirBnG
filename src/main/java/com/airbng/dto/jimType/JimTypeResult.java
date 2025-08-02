7ca684de1ea84aa5785084b87754c4a0ed5925d8
Mouon <ahemsapsldk@gmail.com>
2025-08-02 14:50:18 +0900

    feat : LockerPreviewResult 에 정팩매 추가

--- src/main/java/com/airbng/dto/jimType/JimTypeResult.java
+++ src/main/java/com/airbng/dto/jimType/JimTypeResult.java
@@ -1,5 +1,6 @@
 package com.airbng.dto.jimType;
 
+import com.airbng.domain.jimtype.LockerJimType;
 import lombok.*;
 
 @Getter
@@ -10,4 +11,10 @@ import lombok.*;
 public class JimTypeResult {
     private Long jimTypeId;
     private String typeName;
+    public static JimTypeResult from(LockerJimType lockerJimType){
+        return JimTypeResult.builder()
+                .jimTypeId(lockerJimType.getJimType().getJimTypeId())
+                .typeName(lockerJimType.getJimType().getTypeName())
+                .build();
+    }
 }
--- src/main/java/com/airbng/dto/locker/LockerPreviewResult.java
+++ src/main/java/com/airbng/dto/locker/LockerPreviewResult.java
@@ -1,9 +1,12 @@
 package com.airbng.dto.locker;
 
+import com.airbng.domain.Locker;
 import com.airbng.dto.jimType.JimTypeResult;
+import com.airbng.dto.jimType.LockerJimTypeResult;
 import lombok.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -19,4 +22,21 @@ public class LockerPreviewResult {
     private Double latitude;
     private Double longitude;
     private List<JimTypeResult> jimTypeResults;
+
+    public static LockerPreviewResult from(Locker locker) {
+        return LockerPreviewResult.builder()
+                .lockerId(locker.getLockerId())
+                .lockerName(locker.getLockerName())
+                .address(locker.getAddress())
+                .url(locker.getLockerImages().stream()
+                        .findFirst()
+                        .map(lockerImage -> lockerImage.getImage().getUrl())
+                        .orElse(null))
+                .jimTypeResults(
+                        locker.getLockerJimTypes().stream()
+                                .map(JimTypeResult::from)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
