d3458b11c8d10f90da73c7a4c8e9dfbac340229c
Mouon <ahemsapsldk@gmail.com>
2025-08-09 16:29:56 +0900

    feat : 레디스 RedisTemplate 추가

--- src/main/java/com/airbng/config/RedisConfig.java
+++ src/main/java/com/airbng/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.airbng.config;
 
+import com.airbng.dto.locker.LockerTop5Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,4 +23,21 @@ public class RedisConfig {
 
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
+
+    @Bean
+    public RedisTemplate<String, LockerTop5Response> lockerTop5RedisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, LockerTop5Response> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        Jackson2JsonRedisSerializer<LockerTop5Response> serializer = new Jackson2JsonRedisSerializer<>(LockerTop5Response.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
 }
