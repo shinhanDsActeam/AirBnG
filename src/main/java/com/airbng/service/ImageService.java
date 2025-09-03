cbb772bae16ad1cff47d7c9f3f61062cbd5f812a
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-09-03 12:20:29 +0900

    feat : s3 이미지 삭제
    이미지 업로드 시 s3 기존 이미지 삭제 기능 추가

--- src/main/java/com/airbng/service/ImageService.java
+++ src/main/java/com/airbng/service/ImageService.java
@@ -3,8 +3,9 @@ package com.airbng.service;
 import com.airbng.domain.image.Image;
 import org.springframework.web.multipart.MultipartFile;
 
+
 public interface ImageService {
     Image uploadProfileImage(MultipartFile file);
     Image getDefaultProfileImage();
-    Image updateDefaultProfileImage(MultipartFile file, Long memberId);
+    Image updateProfileImage(MultipartFile file, Long memberId);
 }
