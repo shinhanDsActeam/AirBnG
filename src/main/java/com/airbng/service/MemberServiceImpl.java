4435630cf012271cef1ba5bd99fe1344892a98e7
CHYHEE <151823186+CHYHEE@users.noreply.github.com>
2025-09-04 16:33:13 +0900

    refactor : 리뷰 반영
    하이픈 유무 상관없이 전화번호 중복 방지

--- src/main/java/com/airbng/service/MemberServiceImpl.java
+++ src/main/java/com/airbng/service/MemberServiceImpl.java
@@ -95,14 +95,17 @@ public class MemberServiceImpl implements MemberService {
         if (!userId.equals(request.getMemberId())) {
             throw new MemberException(FAILED_UPDATE_MEMBER);
         }
+
+        String phone = request.getPhone().replace("-", "");
+
         // 닉네임 중복 체크
         if (!member.getNickname().equals(request.getNickname()) &&
                 memberRepository.existsByNickname(request.getNickname())) {
             throw new MemberException(DUPLICATE_NICKNAME);
         }
         // 휴대폰 중복 체크
-        if (member.getPhone().equals(request.getPhone()) &&
-                memberRepository.existsByPhone(request.getPhone())) {
+        if (!member.getPhone().equals(phone) &&
+                memberRepository.existsByPhone(phone)) {
             throw new MemberException(DUPLICATE_PHONE);
         }
 
