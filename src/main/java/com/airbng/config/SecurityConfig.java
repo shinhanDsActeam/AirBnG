d1c2bb2d0bfbdc864c2408f7f20f579809a9acae
standha <myyun02@naver.com>
2025-09-10 17:46:23 +0900

    refactor : 보관소 상세 조회 시 인증/인가 제외

--- src/main/java/com/airbng/config/SecurityConfig.java
+++ src/main/java/com/airbng/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
                                 "/login", "/members/signup", "/reissue",
                                 "/members/check-email","/members/check-nickname",
                                 "/swagger-ui/**","/swagger-resources/**", "/v3/api-docs/**",
-                                "swagger-ui.html","/webjars/**", "/lockers", "/lockers/popular", "/error")
+                                "swagger-ui.html","/webjars/**", "lockers/{lockerId}", "/lockers",
+                                "/lockers/popular", "/error")
 
                         .permitAll()
                         .anyRequest().authenticated()
