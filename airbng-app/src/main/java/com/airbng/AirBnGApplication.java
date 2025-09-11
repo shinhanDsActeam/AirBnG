4e8ff80095bb04d277269533f724537b9a499503
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-11 09:30:49 +0900

    feat : 멀티 모듈 1차 분리
    우선 2개의 모듈로 분리
    - 실행만을 담당하는 airbng-app 모듈
    - 나머지를 담당하는 airbng-core 모듈

--- airbng-app/src/main/java/com/airbng/AirBnGApplication.java
+++ airbng-app/src/main/java/com/airbng/AirBnGApplication.java
@@ -2,8 +2,6 @@ package com.airbng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class AirBnGApplication {
