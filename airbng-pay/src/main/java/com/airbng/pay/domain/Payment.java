a2531849bb815fff39030a94216bb9f12c9daa02
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-15 22:10:38 +0900

    feat : Payment 도메인 클래스 및 상태 enum 생성
    paymentStatus, payMethod등 결제 관련 enum 정의

---
+++ airbng-pay/src/main/java/com/airbng/pay/domain/Payment.java
@@ -0,0 +1,34 @@
+package com.airbng.pay.domain;
+
+import com.airbng.common.base.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @Column(nullable = false)
+    private Long payerId; // 결제자 : dropper
+
+    @Column(nullable = false)
+    private Long payeeId; // 피결제자 : keeper
+
+    @Column(nullable = false)
+    private PayMethod method; // WALLET, PG
+
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus; // 결제상태
+
+    @Column(nullable = false)
+    private String paymentAmount; // 결제 금액
+
+    @Column(nullable = false)
+    private String payIdemKey; // 멱등키
+}
