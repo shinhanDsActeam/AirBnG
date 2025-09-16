87135bd9bd32faed923e9f024fc8d9e7d694ca0b
CHYHEE <wnn2011@naver.com>
2025-09-16 14:25:47 +0900

    feat : 보관소 심사 관련 도메인 추가

--- airbng-admin/src/main/java/com/airbng/admin/domain/PendingLocker.java
+++ airbng-admin/src/main/java/com/airbng/admin/domain/PendingLocker.java
@@ -1,11 +1,63 @@
 package com.airbng.admin.domain;
 
+import com.airbng.admin.domain.base.LockerType;
+import com.airbng.common.base.BaseStatus;
 import com.airbng.common.base.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PendingLocker extends BaseTime {
+
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pendingLockerId;
+
+    @Column(nullable = false)
+    private String lockerName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String addressEnglish;
+
+    @Column(nullable = false)
+    private String addressDetail;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    // TODO : 양방향 1:1 일 경우 fetch = LAZY 설정 적용 안되는듯?
+    @OneToOne(mappedBy = "pendingLocker", cascade = CascadeType.ALL)
+    private LockerReview lockerReview;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    private BaseStatus status;
+
+    @OneToMany(mappedBy = "pendingLocker")
+    private Set<PendingLockerImage> pendingLockerImages;
+
+    @OneToMany(mappedBy = "pendingLocker")
+    @Builder.Default
+    private Set<PendingLockerJimtype> pendingLockerJimtypes = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LockerType lockerType;
+
+    @Column(nullable = false)
+    private Long memberId;
 }
