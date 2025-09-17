bbb62f8cc072545437c9f29f63e066f99c34fc25
standha <myyun02@naver.com>
2025-09-17 09:22:10 +0900

    fix : 보관소 목록에 날짜 나오게 수정

--- airbng-admin/src/main/java/com/airbng/admin/dto/response/LockerReviewListResponse.java
+++ airbng-admin/src/main/java/com/airbng/admin/dto/response/LockerReviewListResponse.java
@@ -4,6 +4,7 @@ import com.airbng.admin.domain.LockerReview;
 import com.airbng.admin.domain.PendingLocker;
 import com.airbng.admin.domain.base.ReviewStatus;
 import com.airbng.api.consumer.dto.view.LockerReviewMemberView;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,7 +19,10 @@ public class LockerReviewListResponse {
     private Long lockerReviewId;
     private String lockerName;
     private String address;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
+
     private ReviewStatus reviewStatus;
     private Long memberId;
     private String memberName;
