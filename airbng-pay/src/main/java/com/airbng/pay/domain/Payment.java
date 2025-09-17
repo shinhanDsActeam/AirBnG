5644b4399f955d94649f2c44ddd3bff1439b7179
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-17 14:29:37 +0900

    fix : 금액 관련 컬럼들 수정
    자릿수 지정 : BigDecimal(19,2)

--- airbng-pay/src/main/java/com/airbng/pay/domain/Payment.java
+++ airbng-pay/src/main/java/com/airbng/pay/domain/Payment.java
@@ -31,8 +31,11 @@ public class Payment extends BaseTime {
     @Column(nullable = false)
     private PaymentStatus paymentStatus; // 결제상태
 
-    @Column(nullable = false)
+    @Column(nullable = false, precision = 19, scale = 2)
     private BigDecimal paymentAmount; // 결제 금액
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal paymentFee; // 수수료
 
     @Column(columnDefinition = "BINARY(16)", nullable = false, unique = true)
     private UUID payIdemKey; // 멱등키
