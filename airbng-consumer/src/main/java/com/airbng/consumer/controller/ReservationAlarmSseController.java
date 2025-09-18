6d5fa088fede887e5c4bded5f6ea11b415a17b1d
standha <myyun02@naver.com>
2025-09-18 17:12:25 +0900

    fix : 알림 전송 메시지 수정

--- airbng-consumer/src/main/java/com/airbng/consumer/controller/ReservationAlarmSseController.java
+++ airbng-consumer/src/main/java/com/airbng/consumer/controller/ReservationAlarmSseController.java
@@ -16,7 +16,7 @@ public class ReservationAlarmSseController {
     private final ReservationAlarmSseService reservationAlarmSseService;
     private final JwtUtil jwtUtil;
 
-    @GetMapping(value = "/reservations/alarms", produces = "text/event-stream;charset=UTF-8")
+    @GetMapping(value = "/reservations/alarms")
     public SseEmitter subscribe(
             @RequestHeader(value = "Last-Event-ID", required = false) String lastEventId,
             @CookieValue(value = "sse", required = false) String sseToken
