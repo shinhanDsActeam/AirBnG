61e9ccd4283b138a4a3336dffe5c428453717cc5
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2025-09-19 02:04:54 +0900

    feat : 지갑 비관적 락 적용 - 락 순서 보장으로 데드락 방지
    지갑 ID를 오름차순으로 정렬한 뒤 순차적으로 락을 걸도록 함.
    개별적으로 락을 걸 경우 락 획득 순서 차이로 데드락이 발생할 수 있어,
    리스트로 묶어 정해진 순서대로 처리하는 방식으로 데드락을 방지함.

--- airbng-pay/src/main/java/com/airbng/pay/usecase/PayApiImpl.java
+++ airbng-pay/src/main/java/com/airbng/pay/usecase/PayApiImpl.java
@@ -5,6 +5,7 @@ import com.airbng.api.pay.dto.command.MakePaymentRequest;
 import com.airbng.api.pay.dto.command.WalletCreateCommand;
 import com.airbng.pay.domain.*;
 import com.airbng.pay.exception.PaymentException;
+import com.airbng.pay.exception.WalletException;
 import com.airbng.pay.repository.PaymentRepository;
 import com.airbng.pay.repository.WalletRepository;
 import com.airbng.pay.util.UUIDUtil;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.airbng.platform.common.response.status.BaseResponseStatus.*;
@@ -69,7 +72,33 @@ class PayApiImpl implements PayApi {
      * @return
      */
     private Long payByWallet(MakePaymentRequest request, UUID payIdemKey) {
+
+        // 결제 금액, 수수료, 총액
+        BigDecimal amount = BigDecimal.valueOf(request.getAmount());
+        BigDecimal fee = BigDecimal.valueOf(request.getFee());
+        BigDecimal totalAmount = amount.add(fee);
+
+        // 락 대상 지갑 목록 구성
+        List<Long> memberIds = new ArrayList<>(List.of(request.getPayerId(), request.getPayeeId()));
+        if (fee.compareTo(BigDecimal.ZERO) > 0) {
+            memberIds.add(ADMIN_MEMBER_ID);
+        }
+
+        // 대상 지갑 락
+        List<Wallet> wallets = walletRepository.findWalletsWithLockByMemberIds(memberIds);
+
+        Wallet payerWallet = walletPickByMemberIdOrThrow(wallets, request.getPayerId());
+        Wallet payeeWallet = walletPickByMemberIdOrThrow(wallets, request.getPayeeId());
     }
+
+    private Wallet walletPickByMemberIdOrThrow(List<Wallet> wallets, Long memberID) {
+        return wallets.stream()
+                .filter(w -> w.getMemberId().equals(memberID))
+                .findFirst()
+                .orElseThrow(()-> new WalletException(INVALID_WALLET));
+    }
+
+
     private Long payByPG(MakePaymentRequest request, UUID payIdemKey) {
         // TODO : PayMethod.PG 결제 처리 - 외부 PG 연동 필요
         throw new PaymentException(UNSUPPORTED_PAY_METHOD);
