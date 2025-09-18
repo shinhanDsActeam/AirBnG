5f6eb8069f19f7a0f9935d9e2ca093820dc2ebee
standha <myyun02@naver.com>
2025-09-18 15:10:08 +0900

    fix : 알림 한글 깨짐 방지 추가

--- airbng-consumer/src/main/java/com/airbng/consumer/controller/ReservationAlarmSseController.java
+++ airbng-consumer/src/main/java/com/airbng/consumer/controller/ReservationAlarmSseController.java
@@ -16,7 +16,7 @@ public class ReservationAlarmSseController {
     private final ReservationAlarmSseService reservationAlarmSseService;
     private final JwtUtil jwtUtil;
 
-    @GetMapping("/reservations/alarms")
+    @GetMapping(value = "/reservations/alarms", produces = "text/event-stream;charset=UTF-8")
     public SseEmitter subscribe(
             @RequestHeader(value = "Last-Event-ID", required = false) String lastEventId,
             @CookieValue(value = "sse", required = false) String sseToken
